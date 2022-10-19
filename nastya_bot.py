840e9dc115e122a97b70bc0a3a4908674036f575
Riceshark <enotpetya@gmail.com>
2022-10-19 12:17:47 +0500

    Add files via upload

---
+++ nastya_bot.py
@@ -0,0 +1,15 @@
+from telegram import bot
+import requests
+
+
+def calc(a: int, b: int) -> int:
+    return a + b
+
+
+def main_loop():
+    while True:
+        ...
+
+
+if __name__ == '__main__':
+    main_loop()
