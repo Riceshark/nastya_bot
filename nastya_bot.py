cecfd225315b43446047d8e56c30cac28a8fe241
Nadezhda <n.zagvozkina@innopolis.ru>
2022-10-21 00:12:23 +0500

    ..

--- nastya_bot.py
+++ nastya_bot.py
@@ -1,60 +1,142 @@
-from telegram import bot
-import requests
+import telebot
+import random
 
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+bot = telebot.TeleBot('5628233116:AAGKEtotlCtyFmAq19RvNxbue3x6YlUUSC8')
+from telebot import types
 
-updater = Updater("5628233116:AAGKEtotlCtyFmAq19RvNxbue3x6YlUUSC8",
-                  use_context=True)
+first = ["Today is the perfect day for new beginnings.",
+         "The optimal day to decide on a bold act!",
+         "Be careful, today the stars can affect your financial condition.",
+         "The best time to start a new relationship or sort out old ones.",
+         "A fruitful day to sort out the accumulated cases."]
 
+second = ["But remember that even in this case, you should not forget about",
+          "If you go out of town, think about it in advance",
+          "Those who are aiming to do a lot of things today should remember about",
+          "If you have a breakdown, pay attention to",
+          "Remember that thoughts are material, which means that you need to constantly think about"]
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Hello sir, Welcome to the Bot.Please write\
-        /help to see the commands available.")
+second_add = ["relationships with friends and family.",
+              "work and business issues that can interfere with plans so inopportunely.",
+              "yourself and your health, otherwise a complete breakdown is possible by the evening.",
+              "household issues — especially those that you did not finish yesterday.",
+              "rest, so as not to turn yourself into a hunted horse at the end of the month."]
 
-def calc(update: Update, context: CallbackContext):
-    try:
-        number1 = int(context.args[0])
-        number2 = int(context.args[1])
-        result = number1+number2
-        update.message.reply_text('The sum is: '+str(result))
-    except (IndexError, ValueError):
-        update.message.reply_text('There are not enough numbers')
-
-
-def help(update: Update, context: CallbackContext):
-    update.message.reply_text("""Available Commands :-
-    /calc number1 number2 - To sum two numbers""")
-
-def unknown(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Sorry '%s' is not a valid command" % update.message.text)
-
-def unknown_text(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Sorry I can't recognize you , you said '%s'" % update.message.text)
+third = ["Evil tongues may tell you the opposite, but you don't need to listen to them today.",
+         "Know that success favors only the persistent, so dedicate this day to the education of the spirit.",
+         "Even if you cannot reduce the influence of retrograde Mercury, then at least bring things to an end.",
+         "There is no need to be afraid of lonely meetings — today is the time when they mean a lot.",
+         "If you meet a stranger on the way, take part, and then this meeting will promise you pleasant troubles."]
 
 
-def main_loop():
-    while True:
-        updater = Updater("5628233116:AAGKEtotlCtyFmAq19RvNxbue3x6YlUUSC8", use_context=True)
-        dp = updater.dispatcher
-        dp.add_handler(CommandHandler('start', start))
-        dp.add_handler(CommandHandler('help', help))
-        dp.add_handler(CommandHandler('calc', calc))
+def command_answer(message: str) -> str:
+    if message == "/start":
+        return "Hi! I can tell you your horoscope for today"
+    elif message == "/help":
+        return "This bot can predict your horoscope for today. To get started, write /start"
+    else:
+        return "I don't understand you. Please write /help."
 
-        dp.add_handler(MessageHandler(Filters.text, unknown))
-        dp.add_handler(MessageHandler(
-            Filters.command, unknown))
-        dp.add_handler(MessageHandler(Filters.text, unknown_text))
-        updater.start_polling()
-        updater.idle()
+
+def generate_advice() -> str:
+    return random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(
+        second_add) + ' ' + random.choice(third)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+
+        bot.send_message(message.from_user.id, command_answer("/start"))
+
+        keyboard = types.InlineKeyboardMarkup()
+
+        # По очереди готовим текст и обработчик для каждого знака зодиака
+
+        key_aries = types.InlineKeyboardButton(text='Aries', callback_data='zodiac')
+
+        # И добавляем кнопку на экран
+
+        keyboard.add(key_aries)
+
+        key_taurus = types.InlineKeyboardButton(text='Taurus', callback_data='zodiac')
+
+        keyboard.add(key_taurus)
+
+        key_gemini = types.InlineKeyboardButton(text='Gemini', callback_data='zodiac')
+
+        keyboard.add(key_gemini)
+
+        key_cancer = types.InlineKeyboardButton(text='Cancer', callback_data='zodiac')
+
+        keyboard.add(key_cancer)
+
+        key_leo = types.InlineKeyboardButton(text='Leo', callback_data='zodiac')
+
+        keyboard.add(key_leo)
+
+        key_virgo = types.InlineKeyboardButton(text='Virgo', callback_data='zodiac')
+
+        keyboard.add(key_virgo)
+
+        key_libra = types.InlineKeyboardButton(text='Libra', callback_data='zodiac')
+
+        keyboard.add(key_libra)
+
+        key_scorpio = types.InlineKeyboardButton(text='Scorpio', callback_data='zodiac')
+
+        keyboard.add(key_scorpio)
+
+        key_sagittarius = types.InlineKeyboardButton(text='Sagittarius', callback_data='zodiac')
+
+        keyboard.add(key_sagittarius)
+
+        key_capricorn = types.InlineKeyboardButton(text='Capricorn', callback_data='zodiac')
+
+        keyboard.add(key_capricorn)
+
+        key_aquarius = types.InlineKeyboardButton(text='Aquarius', callback_data='zodiac')
+
+        keyboard.add(key_aquarius)
+
+        key_pisces = types.InlineKeyboardButton(text='Pisces', callback_data='zodiac')
+
+        keyboard.add(key_pisces)
+
+        # Показываем все кнопки сразу и пишем сообщение о выборе
+
+        bot.send_message(message.from_user.id, text="Choose your zodiac sign:", reply_markup=keyboard)
+
+    else:
+        bot.send_message(message.from_user.id, command_answer(message.text))
+
+
+# Обработчик нажатий на кнопки
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    # Если нажали на одну из 12 кнопок — выводим гороскоп
+
+    if call.data == "zodiac":
+        # Формируем гороскоп
+
+        msg = generate_advice()
+
+        # Отправляем текст в Телеграм
+
+        bot.send_message(call.message.chat.id, msg)
+
+
+# Запускаем постоянный опрос бота в Телеграме
+
+
+def main():
+        bot.polling(none_stop=True, interval=0)
 
 
 if __name__ == '__main__':
-    main_loop()
+    main()
+
+
+def calc(a: int, b: int) -> int:
+    return a + b
--- tests/test_nastya_bot.py
+++ tests/test_nastya_bot.py
@@ -1,14 +1,26 @@
 from unittest import TestCase
 
-from project_212 import calc
+from nastya_bot import command_answer
+from nastya_bot import generate_advice
+from nastya_bot import first
+from nastya_bot import second
+from nastya_bot import third
+from nastya_bot import second_add
 
 
-class TestProject212(TestCase):
-    def test_calc(self) -> None:
+class TestNastyaBot(TestCase):
+    def test_commands(self) -> None:
         cases = [
-            (3, (1, 2)),
-            (0, (-1, 1)),
+            ("Hi! I can tell you your horoscope for today", "/start"),
+            ("This bot can predict your horoscope for today. To get started, write /start", "/help"),
+            ("I don't understand you. Please write /help.", "bla bla bla")
         ]
         for expected, args in cases:
             with self.subTest(expected):
-                self.assertEqual(expected, calc(*args))
+                self.assertEqual(expected, command_answer(args))
+
+    def test_advice(self) -> None:
+        advices_list = first + second + second_add + third
+        check_if_contains = any(substring in generate_advice() for substring in advices_list)
+        self.assertTrue(check_if_contains)
+
